e0b4935307fccc283f2956c8dd11843e63d846f3
Progress Munoriarwa <mmunoria@mtu.edu>
2026-02-02 06:37:24 +0000

    all files removed

--- build/dead_reckoning/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vboxuser/proj3_ws/install/dead_reckoning'
--- build/dead_reckoning/share/dead_reckoning/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/vboxuser/proj3_ws/build/dead_reckoning"
